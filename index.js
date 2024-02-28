fdfc9b4129552cd0b3dda79d9316b60359312549
Mohan-Official <mohana.navaneetha@indiumsoft.com>
2024-02-28 15:41:26 +0530

    Updated file with changes

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
-const ejs = require("ejs");
-app.set("view engine","ejs");
 
 const path = require("path");
-const fs = require("fs/promises");
-
 const port = 3500;
+
 app.use(express.static('public'));
 
-app.get("/",(req,res)=>
+app.get("/summary",(req,res)=>
 {
-    const jsonData = fs.readFile("consolidatedSummary_NON_MRIO_Projects.json", "utf-8");
-    const data = JSON.parse(jsonData);
-    res.sendFile(path.join(__dirname, '/ConsolidatedSummaryPage_NON_MRIO_Projects.html'),{data});
+    res.sendFile(path.join(__dirname, '/ConsolidatedSummaryPage_NON_MRIO_Projects.html'));
 })
 
 app.listen(port,()=>
